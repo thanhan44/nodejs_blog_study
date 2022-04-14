b13d7b64400f9b2a10cdc05ce3e8b547421b90ca
anpt <phanthanhan1999@gmail.com>
2022-04-14 22:20:22 +0700

    Create new course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-  name: { type: String, maxLength: 255 },
-  title: { type: String },
-  description: { type: String, maxLength: 600 },
-  image: { type: String },
-  slug: { type: String },
-  createdAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+  {
+    name: { type: String, maxLength: 255 },
+    title: { type: String },
+    description: { type: String, maxLength: 600 },
+    image: { type: String },
+    videoId: { type: String, maxLength: 255 },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
+    // createdAt: { type: Date, default: Date.now },
+    // updateAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Course", Course);
